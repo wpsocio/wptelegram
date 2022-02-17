956cc326255ff34540b42ff654fd6e3332f54e36
GitHub Action <github-action@users.noreply.github.com>
2022-02-17 11:04:39 +0000

    Deployed from wpsocio/wp-plugins@ac2a0a96d1046e2d7b46f3f2257da1eaa168247f

---
+++ src/assets/static/js/wptelegram--p2tg-classic.4c47884b.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '71dc0c79df5fea5e9ba59c4b83e38203');
---
+++ src/assets/static/js/wptelegram--p2tg-gb.eebd2107.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-plugins'), 'version' => 'f8f33535d5533d00252c1ce098f458ae');
