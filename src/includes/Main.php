d65a22e4e1246b28235542edab633c79ba442312
Irshad Ahmad <irshad@wpsocio.com>
2023-01-19 10:29:14 +0530

    Move wptelegram-bot-api to bot-api

--- src/includes/Main.php
+++ src/includes/Main.php
@@ -196,7 +196,7 @@ final class Main {
 		/**
 		 * The class responsible for loading \WPTelegram\BotAPI library
 		 */
-		require_once $this->dir( '/includes/wptelegram-bot-api/src/index.php' );
+		require_once $this->dir( '/includes/bot-api/src/index.php' );
 
 		/**
 		 * The class responsible for loading \WPTelegram\FormatText library
