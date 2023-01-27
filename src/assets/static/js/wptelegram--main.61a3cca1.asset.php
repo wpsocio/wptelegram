f962a287484a80bc1d7c5f748455de2ac99cffe8
Irshad Ahmad <irshad@wpsocio.com>
2023-01-27 09:33:18 +0000

    Deployed from wpsocio/wp-plugins@7867585382b18137f6c38cc356628397017656e1

---
+++ src/assets/static/js/wptelegram--main.61a3cca1.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => '47577f0d92e35eb5cc12b3010def5fe2');
--- src/assets/static/js/wptelegram--main.89ae84a3.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill'), 'version' => 'ed7b53f873aa8708f864931a613fe382');
